75f1146068bcbeac281008dd6979c362c1dc39af
4ON4 <adolp624@gmail.com>
2021-07-09 13:02:05 +0900

    all files upload

---
+++ include/define.php
@@ -0,0 +1,10 @@
+<?php    
+    define('DBhost','localhost');
+    define('DBuser','aze624');
+    define('DBpass','ay2111!!');
+    define('DBname','aze624');
+    //define('DBhost','ashram777.cafe24.com');
+    //define('DBuser','ashram777');
+    //define('DBpass','dhanstjd77!');
+    //define('DBname','ashram777');
+?>
